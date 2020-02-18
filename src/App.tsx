dd9fc80fa9b40c3060d1c08d8efddfb0479fd2d8
Matt Masters <Matt.d.m@hotmail.com>
2020-02-18 00:49:55 -0600

    improved mobile nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,80 +1,90 @@
-import * as React from "react";
-import { Route, Link } from "react-router-dom";
-import { Header } from "./Header";
-import "./app.scss";
+import * as React from 'react';
+import { Route, Link } from 'react-router-dom';
+import { Header } from './Header';
+import howard from './howard.jpg';
+import './app.scss';
 
 export default function App() {
-  return (
-    <div id="App">
-      <Header />
-      <Route path="/" component={Home} exact />
-      <footer>
-        <span>Copyright 2020</span>
-        <Link to="/about-page">About</Link>
-      </footer>
-    </div>
-  );
+    return (
+        <div id="App">
+            <Header />
+            <Route path="/" component={Home} exact />
+            <Route path="/secret" component={Secret} />
+            <footer>
+                <span>Copyright 2020</span>
+                <Link to="/about-page">About</Link>
+            </footer>
+        </div>
+    );
+}
+
+function Secret() {
+    return (
+        <div className="container">
+            <img src={howard} alt="my dog howard" />
+        </div>
+    );
 }
 
 function Home() {
-  return (
-    <div className="container">
-      <div id="skills">
-        <div className="comment"># full stack web developer</div>
-        <div className="comment"># dallas, tx</div>
-        <div className="comment"># FREELANCE</div>
-        <div className="comment"># SEEKING EMPLOYMENT</div>
-        <br />
+    return (
+        <div className="container">
+            <div id="skills">
+                <div className="comment"># full stack web developer</div>
+                <div className="comment"># dallas, tx</div>
+                <div className="comment"># FREELANCE</div>
+                <div className="comment"># SEEKING EMPLOYMENT</div>
+                <br />
 
-        <div className="comment"># skills</div>
-        <Func name="languages">
-          <ul>
-            <li>Javascript / TypeScript</li>
-            <li />
-            <li>Python</li>
-            <li>CSS / SCSS / LESS</li>
-            <li>SQL</li>
-            <li>HTML</li>
-          </ul>
-        </Func>
-        <Func name="frameworks">
-          <ul>
-            <li>Node / Express</li>
-            <li>Flask</li>
-            <li>React</li>
-            <li>Django </li>
-          </ul>
-        </Func>
-        <Func name="other">
-          <ul>
-            <li>Linux</li>
-            <li>BASH / ZSH</li>
-            <li>Webpack / Parcel / Rollup</li>
-          </ul>
-        </Func>
-      </div>
-    </div>
-  );
+                <div className="comment"># skills</div>
+                <Func name="languages">
+                    <ul>
+                        <li>Javascript / TypeScript</li>
+                        <li />
+                        <li>Python</li>
+                        <li>CSS / SCSS / LESS</li>
+                        <li>SQL</li>
+                        <li>HTML</li>
+                    </ul>
+                </Func>
+                <Func name="frameworks">
+                    <ul>
+                        <li>Node / Express</li>
+                        <li>Flask</li>
+                        <li>React</li>
+                        <li>Django </li>
+                    </ul>
+                </Func>
+                <Func name="other">
+                    <ul>
+                        <li>Linux</li>
+                        <li>BASH / ZSH</li>
+                        <li>Webpack / Parcel / Rollup</li>
+                    </ul>
+                </Func>
+            </div>
+        </div>
+    );
 }
 
 interface FuncProps {
-  name: string;
-  children: React.ReactNode;
+    name: string;
+    children: React.ReactNode;
 }
 
 function Func({ name, children }: FuncProps) {
-  return (
-    <>
-      <span className="func">
-        <span className="const">const</span>
-        <span className="name">{name}</span>
-        <span className="equals">{"="}</span>
-        <span className="parens">{"()"}</span>
-        <span className="arrow">{"=>"}</span>
-        <span className="bracket">{"{"}</span>
-      </span>
-      {children}
-      <span className="bracket">{"}"}</span>
-    </>
-  );
+    return (
+        <>
+            <span className="func">
+                <span className="const">const</span>
+                <span className="name">{name}</span>
+                <span className="equals">{'='}</span>
+                <span className="parens">{'()'}</span>
+                <span className="arrow">{'=>'}</span>
+                <span className="bracket">{'{'}</span>
+            </span>
+            {children}
+            <span className="bracket">{'}'}</span>
+        </>
+    );
 }
--- src/Header.tsx
+++ src/Header.tsx
@@ -1,47 +1,50 @@
-import React from "react";
-import { NavLink, useHistory } from "react-router-dom";
-import "./app.scss";
+import React from 'react';
+import { NavLink, useHistory } from 'react-router-dom';
+import './app.scss';
 
 export function Header() {
-  let [clicks, setClicks] = React.useState(0);
-  let history = useHistory();
-  function click() {
-    if (clicks < 5) {
-      setClicks(clicks + 1);
-    } else {
-      history.push("/secret");
-      setClicks(0);
+    let [clicks, setClicks] = React.useState(0);
+    let history = useHistory();
+    function click() {
+        if (clicks < 5) {
+            setClicks(clicks + 1);
+        } else {
+            history.push('/secret');
+            setClicks(0);
+        }
     }
-  }
-  return (
-    <header>
-      <h1 onClick={click}>Matt Masters.</h1>
-      <nav>
-        <NavTab to="/">{["HOME", "HELLO WORLD"]}</NavTab>
-        <NavTab to="/lab">{["LAB", "CODE Experiments"]}</NavTab>
-        <NavTab to="/gallery">{["GALLERY", "SELECTED PROJECTS"]}</NavTab>
-      </nav>
-    </header>
-  );
+    return (
+        <header>
+            <h1 onClick={click}>Matt Masters.</h1>
+            <nav>
+                <NavTab to="/">{['HOME', 'HELLO WORLD']}</NavTab>
+                <NavTab to="/lab">{['LaB', 'CODE Experiments']}</NavTab>
+                <NavTab to="/gallery">
+                    {['GAllERY', 'SEleCTED PROJECTS']}
+                </NavTab>
+            </nav>
+        </header>
+    );
 }
 
 interface TabProps {
-  children: string[];
-  to: string;
+    children: string[];
+    to: string;
 }
 
 function NavTab({ children, to }: TabProps) {
-  let [index, setIndex] = React.useState(0);
-  function mouseEnter() {
-    setIndex(1);
-  }
-  function mouseLeave() {
-    setIndex(0);
-  }
+    let [index, setIndex] = React.useState(0);
 
-  return (
-    <NavLink onMouseEnter={mouseEnter} onMouseLeave={mouseLeave} to={to}>
-      {children[index]}
-    </NavLink>
-  );
+    function mouseEnter() {
+        setIndex(1);
+    }
+    function mouseLeave() {
+        setIndex(0);
+    }
+
+    return (
+        <NavLink onMouseEnter={mouseEnter} onMouseLeave={mouseLeave} to={to}>
+            {children[index]}
+        </NavLink>
+    );
 }
